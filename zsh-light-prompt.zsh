75f653a58162fcfe472f4ddac5e78cbddc28813d
Thomas Burk <Buckmeister@shellraiser.com>
2020-06-03 16:27:59 +0200

    Add basic plugin layout

---
+++ zsh-light-prompt.zsh
@@ -0,0 +1,33 @@
+# set up PROMPT and RPOMPT
+export PROMPT='%(?.%F{green} ✔ .%F{red} 𝙭 %?)%f %B%F{12}%1~%f%b %# '
+export RPROMPT='$vcs_info_msg_0_ ${vim_mode}'
+
+# set up vcs_info
+autoload -Uz vcs_info
+precmd_vcs_info() { vcs_info }
+precmd_functions+=( precmd_vcs_info )
+zstyle ':vcs_info:*' disable bzr cdv darcs mtn svk tla cvs svn
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' stagedstr "%F{yellow} 𝓼 %f"
+zstyle ':vcs_info:*' unstagedstr "%F{red} 𝓾 %f"
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' check-for-staged-changes true
+zstyle ':vcs_info:git:*' formats '%F{14}%B[%r/%S]%f%u%c%m(%b)'
+zstyle ':vcs_info:git:*' actionformats '%F{14}%B[%r/%S]%f%u%c%m(%b|%a)'
+
+# create vim_mode indicator for RPROMPT
+vim_ins_mode="%F{green}%B𝓲 %f"
+vim_cmd_mode="%F{blue}%B𝓬 %f"
+vim_mode=$vim_ins_mode
+
+function zle-keymap-select {
+  vim_mode="${${KEYMAP/vicmd/${vim_cmd_mode}}/(main|viins)/${vim_ins_mode}}"
+  zle reset-prompt
+}
+
+zle -N zle-keymap-select
+
+function zle-line-finish {
+  vim_mode=$vim_ins_mode
+}
+zle -N zle-line-finish
